8f78a8f2ad74f563f3de1bcf30c3ac3934975856
hiroto takao <biiiy080121@outlook.com>
2024-08-07 08:06:42 +0900

    first commit

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 'use strict';
+import chalk from "chalk";
+import assert from "node:assert";
 /**
  * 与えられた自然数の階乗を返す
  * 階乗とは、1からその与えられた自然数までの数をすべてかけたものです
@@ -7,10 +9,12 @@
  */
 function factorial(n) {
   let result = 1;
-  // TODO このコメントを消して正しく実装してください。
+  for(let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  console.log(chalk.green(result));
   return result;
 }
-const assert = require('node:assert');
 assert.strictEqual(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
 assert.strictEqual(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
 assert.strictEqual(factorial(3), 6, `3の階乗は6ですが、実際は${factorial(3) }でした`);
